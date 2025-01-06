a81443e1f1f86b4d7a3f19a1c84ff9e64bd09576
sictransit <micke@sictransit.net>
2025-01-06 12:47:32 +0100

    working prototype

--- mosquito/mosquito.ino/mosquito.ino.ino
+++ mosquito/mosquito.ino/mosquito.ino.ino
@@ -1,14 +1,11 @@
 #include <ESP8266WiFi.h>
-#define LED 4
 
-int brightness = 0;    // how bright the LED is
-int fadeAmount = 5;    // how many points to fade the LED by
-int sensorValue = 0;
-int sensorPin = A0;
-int analogFrequency = 330;
-int analogRange = 256;
-int frequency = 0;
-int delta = 1;
+#define LED 4
+#define AIN A0
+
+int brightness = 128;    // how bright the LED is
+int fadeAmount = 1;    // how many points to fade the LED by
+int adcValue = 1023;
 
 //=======================================================================
 //                    Power on setup
@@ -16,25 +13,29 @@ int delta = 1;
 void setup() {
   Serial.begin(115200);
   pinMode(LED,OUTPUT);
-  analogWriteFreq(analogFrequency);
-  analogWriteRange(analogRange);
-  frequency = analogFrequency;
+  analogWriteFreq(320);
+  //analogWrite(LED, 200);
 }
 
 //=======================================================================
 //                    Main Program Loop
 //=======================================================================
 void loop() {
-  
-  if (frequency > analogFrequency + 10 || frequency < analogFrequency - 10)
-  {
-    delta *= -1;
-    Serial.println(frequency);
-  }
-  
-  frequency += delta;
-    
-  analogWrite(LED,frequency);
-  delay(1000);  
+    if (brightness <=128-8 || brightness >= 128+8)
+    {
+      fadeAmount*=-1;      
+      adcValue = analogRead(AIN);
+      //Serial.println(adcValue);
+    }
+    brightness += fadeAmount;    
+    if (adcValue > 512)
+    {
+      analogWrite(LED, brightness);
+    }
+    else
+    {
+      analogWrite(LED, 0);
+    }
+    //delay(1);
 }
 //=======================================================================
