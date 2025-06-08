226d2f96d76b20b96832966f540f398d409e0ba9
sictransit <micke@sictransit.net>
2025-06-08 15:39:20 +0200

    getting there

--- mosquito/mosquito/src/app/app.ts
+++ mosquito/mosquito/src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './components/header/header';
+import { Main } from './components/main/main';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, Main],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
